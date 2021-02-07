bec7a926fb881db3c9d8eb7652fac5360be60800
Fabian Helm <fabian.helm@abacus.ac>
2021-02-07 21:26:22 +0100

    nice updates

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <h1 class="tag is-info is-large">Absteige Nuki Logs</h1>
-
+    <div class="columns pt-4 pl-2 ml-1 is-centered">
+      <h1 class="title">Absteige Logs</h1>
+    </div>
     <section class="main-content columns">
       <div class="container column is-10">
         <nuxt keep-alive :keep-alive-props="{ max: 10 }" />
--- pages/abs/index.vue
+++ pages/abs/index.vue
@@ -35,10 +35,11 @@
 
         <b-table-column field="action" label="Action" sortable v-slot="props">
           <b-tooltip v-if="props.row.action == '2'" label="Zugesperrt">
-            <b-icon icon="lock"> </b-icon>
+            <b-icon class="has-text-success" icon="lock"> </b-icon>
           </b-tooltip>
           <b-tooltip v-else-if="props.row.action == '3'" label="Aufgesperrt">
-            <b-icon icon="lock-open-variant-outline"> </b-icon>
+            <b-icon class="has-text-danger" icon="lock-open-variant-outline">
+            </b-icon>
           </b-tooltip>
           <b-tooltip v-else-if="props.row.action == '240'" label="Tür geöffnet">
             <b-icon icon="door-open"> </b-icon>
@@ -66,7 +67,7 @@
         <option value="20">20 per page</option>
       </b-select>
       <div class="control is-flex">
-        <b-switch v-model="isDoorStateShown" @input="filterDoorStates"
+        <b-switch v-model="isDoorStateShown" @input="$fetch"
           >Tür Status anzeigen</b-switch
         >
       </div>
@@ -76,18 +77,10 @@
 
 <script>
 export default {
-  methods: {
-    filterDoorStates() {
-      if (!this.isDoorStateShown) {
-        this.data = this.data.filter((elem) => [2, 3].includes(elem.action))
-      } else {
-        this.$fetch()
-      }
-    },
-  },
   data() {
     return {
       data: [],
+      origData: [],
       isPaginated: true,
       isPaginationSimple: false,
       isPaginationRounded: false,
@@ -100,11 +93,6 @@ export default {
       isDoorStateShown: false,
     }
   },
-  activated() {
-    if (!this.isDoorStateShown) {
-      this.data = this.data.filter((elem) => [2, 3].includes(elem.action))
-    }
-  },
   async fetch() {
     const response = await fetch('https://api.nuki.io/smartlock/log?limit=40', {
       headers: {
@@ -112,7 +100,13 @@ export default {
           'Bearer e9bff49447c33a52746e84751773384b546fcd23d96c519fdf0beef8b1c4af1ec8c21bc185b21f4c',
       },
     })
+    // const response = await fetch('/abs-log-data')
     this.data = await response.json()
+
+    if (!this.isDoorStateShown) {
+      this.data = this.data.filter((elem) => [2, 3].includes(elem.action))
+    }
   },
+  fetchOnServer: false,
 }
 </script>
